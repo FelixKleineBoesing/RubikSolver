060b1af44a35e9d0eca8001e86ebf20381b7470e
Felix Kleine Bösing <felix.boesing@t-online.de>
2019-12-12 22:05:43 +0100

    rotation seems to work now.
    TODO: write some tests for rotation. Implement solver

--- rubiksolver/cube.py
+++ rubiksolver/cube.py
@@ -11,8 +11,8 @@ INDEX_DICT = {0: 0, 1: 2, 2: 0, 3: 2, 4: 0, 5: 2}
 # this dict marks which side is on which axis, 0 is x, 1 is y, 2 is z
 AXIS_DICT = {0: 0, 1: 0, 2: 1, 3: 1, 4: 2, 5: 2}
 # this dict marks which side should turn which axis at what axis
-TRANSITION_AXIS_DICT = {0: {1: 1, 2: 2}, 1: {1: {1: 1, 2: 2}, 2: {0: 1, 1: 2}},
-                        3: {0: 1, 1: 2}, 4: {2: 1, 0: 2}, 5: {2: 1, 0: 2}}
+TRANSITION_AXIS_DICT = {0: {1: 1, 2: 2}, 1: {1: 1, 2: 2}, 2: {2: 1, 0: 2},
+                        3: {2: 1, 0: 2}, 4: {0: 1, 1: 2}, 5: {0: 1, 1: 2}}
 
 
 class Side(Enum):
@@ -47,18 +47,19 @@ class Cube:
             side_before = get_neighbor_before(i, neighbors)
             axis_after = AXIS_DICT[n]
             index = INDEX_DICT[side.value]
-            axix_to_copy_to = TRANSITION_AXIS_DICT[side.value][axis_after]
+            axis_to_copy_to = TRANSITION_AXIS_DICT[side.value][axis_after]
             axis_before = AXIS_DICT[side_before]
             axix_to_copy_from = TRANSITION_AXIS_DICT[n][axis_before]
             LI = [index, index, index]
             LI[0] = n
-            LI[axix_to_copy_to] = [0, 1, 2]
+            LI[axis_to_copy_to] = [0, 1, 2]
             RI = [index, index, index]
             RI[0] = side_before
             RI[axix_to_copy_from] = [0, 1, 2]
 
             cube[tuple(LI)] = self.cube[tuple(RI)]
-        print(cube)
+        self.cube = cube
+
 
 def get_neighbor_before(index: int, neighbors: list):
     """
@@ -76,7 +77,7 @@ def get_neighbor_before(index: int, neighbors: list):
     assert isinstance(neighbors, list)
     assert isinstance(index, int)
     assert index >= 0
-    assert index < len(neighbors) - 1, "index must be smaller than the length of the list neighbors"
+    assert index < len(neighbors), "index must be smaller than the length of the list neighbors"
     if index == 0:
         return neighbors[len(neighbors) - 1]
     else:
@@ -87,3 +88,5 @@ if __name__ == "__main__":
     cube = Cube()
     cube.init_cube()
     cube.rotate(Side.front, Direction.clockwise)
+    cube.rotate(Side.top, Direction.clockwise)
+    print(cube.cube)
