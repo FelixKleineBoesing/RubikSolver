c21dbf5a4a4eb07b7ba8e6fe5da897083f33eb8d
Felix Kleine Bösing <felix.boesing@t-online.de>
2019-12-14 01:10:09 +0100

    implement tests

--- tests/cube_tester.py
+++ tests/cube_tester.py
@@ -1,15 +1,30 @@
 import unittest
 from rubiksolver.cube import Cube, Side, Direction
 import random
-sides = [Side.back, Side.bottom, Side.front, Side.left, Side.right, Side.top]
+import copy
+
+
+all_sides = [Side.back, Side.bottom, Side.front, Side.left, Side.right, Side.top]
+all_directions = [Direction.clockwise, Direction.counter_clockwise]
 
 
 class CubeTester(unittest.TestCase):
 
     def test_naive(self):
         cube = Cube()
-        sides = [random.choice(side) for side in sides]
+        initial_cube = copy.deepcopy(cube)
 
+        reverse_directions = {Direction.counter_clockwise: Direction.clockwise,
+                            Direction.clockwise: Direction.counter_clockwise}
+        sides = [random.choice(all_sides) for _ in range(20)]
+        reversed_sides = list(reversed(sides))
+        directions = [random.choice(all_directions) for _ in range(20)]
+        reversed_directions = [reverse_directions[dir_] for dir_ in directions]
 
+        for direc, side in zip(directions, sides):
+            cube.rotate(side, direc)
 
+        for direc, side in zip(reversed_directions, reversed_sides):
+            cube.rotate(side, direc)
 
+        self.assertEqual(cube, initial_cube)
